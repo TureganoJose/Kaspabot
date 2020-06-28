d2f99aba8b03362b6cb61e9b3daa01e91f428a68
Jose Martinez <turegano.jose@gmail.com>
2020-06-28 12:55:28 +0100

    Just tested servos for the first time

--- Servo_test/helloWorld.py
+++ Servo_test/helloWorld.py
@@ -5,7 +5,7 @@ from math import sin, cos
 # This will be different for different computers
 # On Windows, try the ports COM1, COM2, COM3, etc...
 # On Raspbian, try each port in /dev/
-LX16A.initialize("/dev/ttyUSB0")
+LX16A.initialize("COM3")
 
 # There should two servos connected, with IDs 1 and 2
 servo1 = LX16A(1)
@@ -18,6 +18,7 @@ while True:
 	# The servos can rotate between 0 and 240 degrees,
 	# So we adjust the waves to be in that range
 	servo1.moveTimeWrite(sin(t) * 120 + 120)
+	#print("Servo 1 pos %d" % servo1.getPhysicalPos())
 	servo2.moveTimeWrite(cos(t) * 120 + 120)
-	
+	#print("Servo 2 pos %d" % servo2.getPhysicalPos())
 	t += 0.01
