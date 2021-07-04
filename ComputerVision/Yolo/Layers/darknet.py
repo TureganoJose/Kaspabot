1f1b15e3844de84a3a4beea2ee01489d15329922
Jose Martinez <turegano.jose@gmail.com>
2021-07-04 18:33:09 +0100

    Slowly building a trainable model. Took a dataset from the internet to keep me going.

--- ComputerVision/Yolo/Layers/darknet.py
+++ ComputerVision/Yolo/Layers/darknet.py
@@ -8,7 +8,7 @@ import numpy as np
 import sys
 print(sys.path)
 sys.path.append("../")
-from utils.util import predict_transform
+from Yolo.utils.util import predict_transform, write_results
 import cv2
 
 class EmptyLayer(nn.Module):
@@ -210,10 +210,31 @@ def get_test_input():
     img_ = Variable(img_)                     # Convert to Variable
     return img_
 
-# blocks = parse_cfg(r"C:\Workspaces\Kaspabot\ComputerVision\Yolo\cfg\config.cfg")
-# print(create_modules(blocks))
-#
-# model = Darknet(r"C:\Workspaces\Kaspabot\ComputerVision\Yolo\cfg\config.cfg")
-# inp = get_test_input()
-# pred = model(inp, torch.cuda.is_available())
-# print (pred)
+blocks = parse_cfg(r"C:\Workspaces\Kaspabot\ComputerVision\Yolo\cfg\config.cfg")
+print(create_modules(blocks))
+
+model = Darknet(r"C:\Workspaces\Kaspabot\ComputerVision\Yolo\cfg\config.cfg")
+inp = get_test_input()
+pred = model(inp, torch.cuda.is_available())
+print(pred)
+
+
+confidence = 0.5
+num_classes = 80
+nms_thesh = 0.4
+prediction = write_results(pred, confidence, num_classes, nms_conf=nms_thesh)
+print(prediction)
+
+def write(x, results, color):
+    c1 = tuple(x[1:3].int())
+    c2 = tuple(x[3:5].int())
+    img = results[int(x[0])]
+    cls = int(x[-1])
+    label = "{0}" #.format(classes[cls])
+    cv2.rectangle(img, c1, c2,color, 1)
+    t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 1 , 1)[0]
+    c2 = c1[0] + t_size[0] + 3, c1[1] + t_size[1] + 4
+    cv2.rectangle(img, c1, c2,color, -1)
+    cv2.putText(img, label, (c1[0], c1[1] + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 1, [225,255,255], 1);
+    return img
+
